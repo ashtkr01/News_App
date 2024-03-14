ca778abf4c174478b02ba7abd7f30c97960ca02f
ashtkr01 <ashishkumarkatheriyaa@gmail.com>
2024-03-14 14:24:08 +0530

    Added the functionality : fetching news via API and implement componentDidMount()

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -5,12 +5,12 @@ export default class NewsItem extends Component {
         let {title , description , imageUrl , newsUrl } = this.props;
         return (
             <>
-                <div className="card" style={{width: "18rem;"}}>
-                    <img src={imageUrl} className="card-img-top" alt="..."/>
+                <div className="card" style={{width: "18rem"}}>
+                    <img src={imageUrl ? imageUrl : "https://media.istockphoto.com/id/1369150014/vector/breaking-news-with-world-map-background-vector.jpg?s=1024x1024&w=is&k=20&c=blBt3PJbOSEZF5_zB5YgKYeq9Zx_RMOLntX_nI3lliQ="} className="card-img-top" alt="..."/>
                         <div className="card-body">
-                            <h5 className="card-title">{title}</h5>
-                            <p className="card-text">{description}</p>
-                            <a href="/newDetail" className="btn btn-sm btn-primary">Read More</a>
+                            <h5 className="card-title">{title}...</h5>
+                            <p className="card-text">{description}...</p>
+                            <a href={newsUrl} target = "_blank" className="btn btn-sm btn-primary">Read More</a>
                         </div>
                 </div>
             </>
