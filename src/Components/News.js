b6fce7050e861cbdab5b0c378895f73e22fe507c
ashtkr01 <ashishkumarkatheriyaa@gmail.com>
2024-03-14 20:30:38 +0530

    Making changes with new API and Adding previous and next buttons

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,42 +2,76 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export default class News extends Component {
-  
+
   constructor() {
     super();
     this.state = {
       articles: [],
       loading: false,
+      totalResults: 0,
+      page: 1,
     }
   }
   //Async:
-  async componentDidMount(){
+  async componentDidMount() {
     console.log("inside component did mount");
-    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=ffd32a15b61b4073b3a327daa4412797";
+    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=ffd32a15b61b4073b3a327daa4412797&page=1&pageSize=20";
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({articles : parsedData.articles});
+    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults });
   }
+
+  handleNextClick = async() => {
+    //Valid:
+    if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+      return;
+    }
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=ffd32a15b61b4073b3a327daa4412797&page=${this.state.page + 1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({ articles: parsedData.articles });
+    this.setState({
+      page : this.state.page + 1
+    });
+  }
+
+  handlePrevClick = async() => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=ffd32a15b61b4073b3a327daa4412797&page=${this.state.page - 1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({ articles: parsedData.articles });
+    this.setState({
+      page : this.state.page - 1
+    });
+  }
+
   render() {
     return (
       <div className="container my-4">
         <h2>News App - Top Headlines</h2>
         <div className="row">
-          {this.state.articles && this.state.articles.map((element) => {
-            return <div className="col-md-4" key={element.url}>
-              <NewsItem  
-               title={element.title && element.title.slice(0 , 20)} 
-               description={element.description && element.description.slice(0 , 80)} 
-               imageUrl={element.urlToImage}
+          {this.state.articles && this.state.articles.map((element, index) => {
+            return <div className="col-md-4" key={index}>
+              <NewsItem
+                title={element.title && element.title.slice(0, 20)}
+                description={element.description && element.description.slice(0, 80)}
+                imageUrl={element.urlToImage}
                 newsUrl={element.url} />
             </div>
           })}
-          {/* <div className="col-md-4">
-                        <NewsItem title="Rahul" description="Youth" imageUrl="https://ichef.bbci.co.uk/news/1024/branded_news/1852D/production/_132892699_arielhenry.png"
-                        newsUrl = "Hello"/>
-                    </div> */}
         </div>
+        <div className="d-flex justify-content-between my-4">
+          <button disabled = {this.state.page <= 1} type="button" className="btn btn-secondary" onClick={this.handlePrevClick}>
+            	&larr; Prev
+          </button>
+          <button type="button" className="btn btn-secondary" onClick={this.handleNextClick}>
+            Next &rarr;
+          </button>
+        </div>
+
       </div>
     )
   }
